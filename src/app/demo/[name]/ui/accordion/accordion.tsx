9dbd04a56be7357984f943c2ec358f5fd5c3997c
politokk <ellielinder2023@gmail.com>
2025-11-23 18:48:34 -0500

    accordion

--- src/app/demo/[name]/ui/accordion/accordion.tsx
+++ src/app/demo/[name]/ui/accordion/accordion.tsx
@@ -1,12 +1,15 @@
 import { ComponentWrapper } from "@/components/display/component-wrapper"
 import AccordionDemo from "./accordion-demo";
-
+import AccordionExtensive from "./accordion-extensive";
 export function accordion() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         <ComponentWrapper name="Accordion" icon="ChevronDown">
           <AccordionDemo />
+        </ComponentWrapper>
+        <ComponentWrapper name="Accordion Extensive" icon="ScrollText">
+          <AccordionExtensive />
         </ComponentWrapper>
       </div>
     </div>
