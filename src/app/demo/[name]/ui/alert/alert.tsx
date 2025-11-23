0c4734507c2ff66da6dee42787db956c22a4b7da
politokk <ellielinder2023@gmail.com>
2025-11-23 18:53:27 -0500

    alert

--- src/app/demo/[name]/ui/alert/alert.tsx
+++ src/app/demo/[name]/ui/alert/alert.tsx
@@ -1,6 +1,17 @@
 import { ComponentWrapper } from "@/components/display/component-wrapper"
 import AlertDemo from "./alert-demo"
 import AlertDestructive from "./alert-destructive"
+import AlertCustomColorsDemo from "./alert-custom-colors-demo"
+import AlertSuccessDemo from "./alert-success-demo"
+import AlertDescriptionOnlyDemo from "./alert-description-only-demo"
+import AlertMinimalDemo from "./alert-minimal-demo"
+import AlertTitleOnlyDemo from "./alert-title-only-demo"
+import AlertLongTitleDemo from "./alert-long-title-demo"
+import AlertLongDescriptionDemo from "./alert-long-description-demo"
+import AlertExtensiveContentDemo from "./alert-extensive-content-demo"
+import AlertDestructiveDemo from "./alert-destructive-demo"
+import AlertDestructiveListDemo from "./alert-destructive-list-demo"
+import AlertWithActionDemo from "./alert-with-action-demo"
 
 export function alert() {
   return (
@@ -11,6 +22,39 @@ export function alert() {
         </ComponentWrapper>
         <ComponentWrapper name="Alert Destructive" icon="AlertTriangle">
           <AlertDestructive />
+        </ComponentWrapper>
+        <ComponentWrapper name="Alert Success" icon="CheckCircle2Icon">
+          <AlertSuccessDemo />
+        </ComponentWrapper>
+        <ComponentWrapper name="Alert Description Only" icon="BookmarkCheckIcon">
+          <AlertDescriptionOnlyDemo />
+        </ComponentWrapper>
+        <ComponentWrapper name="Alert Minimal" icon="FileTextIcon">
+          <AlertMinimalDemo />
+        </ComponentWrapper>
+        <ComponentWrapper name="Alert Title Only" icon="PopcornIcon">
+          <AlertTitleOnlyDemo />
+        </ComponentWrapper>
+        <ComponentWrapper name="Alert Long Title" icon="TextIcon">
+          <AlertLongTitleDemo />
+        </ComponentWrapper>
+        <ComponentWrapper name="Alert Long Description" icon="GiftIcon">
+          <AlertLongDescriptionDemo />
+        </ComponentWrapper>
+        <ComponentWrapper name="Alert Extensive Content" icon="LayoutListIcon">
+          <AlertExtensiveContentDemo />
+        </ComponentWrapper>
+        <ComponentWrapper name="Alert Destructive" icon="AlertCircleIcon">
+          <AlertDestructiveDemo />
+        </ComponentWrapper>
+        <ComponentWrapper name="Alert Destructive List" icon="AlertTriangleIcon">
+          <AlertDestructiveListDemo />
+        </ComponentWrapper>
+        <ComponentWrapper name="Alert With Action" icon="MousePointerClickIcon">
+          <AlertWithActionDemo />
+        </ComponentWrapper>
+        <ComponentWrapper name="Alert Custom Colors" icon="PaletteIcon">
+          <AlertCustomColorsDemo />
         </ComponentWrapper>
       </div>
     </div>
