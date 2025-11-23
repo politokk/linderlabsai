3b0d66a6341f197e87fbf20ee4a92d4a1f0003e3
politokk <ellielinder2023@gmail.com>
2025-11-23 15:14:03 -0500

    forms

--- src/app/demo/[name]/ui/form/form.tsx
+++ src/app/demo/[name]/ui/form/form.tsx
@@ -3,6 +3,12 @@ import FormRhfArray from "./form-rhf-array";
 import FormRhfCheckbox from "./form-rhf-checkbox";
 import FormRhfComplex from "./form-rhf-complex";
 import FormRhfDemo from "./form-rhf-demo";
+import FormRhfInput from "./form-rhf-input";
+import FormRhfPassword from "./form-rhf-password";
+import FormRhfRadiogroup from "./form-rhf-radiogroup";
+import FormRhfSelect from "./form-rhf-select";
+import FormRhfSwitch from "./form-rhf-switch";
+import FormRhfTextarea from "./form-rhf-textarea";
 import FormNextDemo from "./form-next-demo";
 export const form = {
   name: "form",
@@ -13,5 +19,11 @@ export const form = {
     FormRhfComplex: <FormRhfComplex/>,
     FormRhfDemo: <FormRhfDemo/>,
     FormNextDemo: <FormNextDemo/>,
+    FormRhfInput: <FormRhfInput/>,
+    FormRhfPassword: <FormRhfPassword/>,
+    FormRhfRadiogroup: <FormRhfRadiogroup/>,
+    FormRhfSelect: <FormRhfSelect/>,
+    FormRhfSwitch: <FormRhfSwitch/>,
+    FormRhfTextarea: <FormRhfTextarea/>,
   },
 };
