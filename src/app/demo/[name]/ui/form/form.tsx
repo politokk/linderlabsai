567014bd8f4a24bd628c26406c06a93c967ba0b9
politokk <ellielinder2023@gmail.com>
2025-11-23 15:12:29 -0500

    forms

--- src/app/demo/[name]/ui/form/form.tsx
+++ src/app/demo/[name]/ui/form/form.tsx
@@ -1,9 +1,17 @@
 import FormNextComplex from "./form-next-complex";
-
+import FormRhfArray from "./form-rhf-array";
+import FormRhfCheckbox from "./form-rhf-checkbox";
+import FormRhfComplex from "./form-rhf-complex";
+import FormRhfDemo from "./form-rhf-demo";
+import FormNextDemo from "./form-next-demo";
 export const form = {
   name: "form",
   components: {
     FormNextComplex: <FormNextComplex/>,
-   
+    FormRhfArray: <FormRhfArray/>,
+    FormRhfCheckbox: <FormRhfCheckbox/>,
+    FormRhfComplex: <FormRhfComplex/>,
+    FormRhfDemo: <FormRhfDemo/>,
+    FormNextDemo: <FormNextDemo/>,
   },
 };
