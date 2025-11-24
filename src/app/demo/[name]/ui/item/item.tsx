a67af4dfd16a01fae907222d5b3cb89e0b93d565
politokk <ellielinder2023@gmail.com>
2025-11-23 20:06:12 -0500

    item invite demo

--- src/app/demo/[name]/ui/item/item.tsx
+++ src/app/demo/[name]/ui/item/item.tsx
@@ -1,5 +1,4 @@
 import ItemDemo from "./item-demo";
-import ItemSizeDemo from "./item-size";
 import ItemVariant from "./item-variant";
 import ItemAvatar from "./item-avatar";
 import ItemDropdown from "./item-dropdown";
@@ -10,16 +9,12 @@ import ItemImage from "./item-image";
 import ItemLink from "./item-link";
 import { ComponentWrapper } from "@/components/display/component-wrapper";
 
-
 export function item() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         <ComponentWrapper name="Item" icon="Box">
           <ItemDemo />
-        </ComponentWrapper>
-        <ComponentWrapper name="Item Size" icon="Maximize">
-          <ItemSizeDemo />
         </ComponentWrapper>
         <ComponentWrapper name="Item Variant" icon="Palette">
           <ItemVariant />
