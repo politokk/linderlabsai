d3fcd97e1f38d23434ce0c3b61a687ad8f20c165
politokk <ellielinder2023@gmail.com>
2025-11-24 21:31:18 -0500

    nav-registry

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,21 +1,39 @@
-import { GalleryVerticalEnd } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
+import { Badge } from "./ui/badge";
 import { SidebarMenuButton } from "./ui/sidebar";
 
 export function Logo() {
   return (
     <div className="flex items-center gap-2">
-     <SidebarMenuButton size="lg" asChild>
-              <Link href="/">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <GalleryVerticalEnd className="size-4" />
-                </div>
-                <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-medium">Documentation</span>
-                  <span className="">v1.0.0</span>
-                </div>
-              </Link>
-            </SidebarMenuButton>
+      <SidebarMenuButton size="lg" asChild>
+        <Link href="/">
+          <div className="flex aspect-square size-8 items-center justify-center rounded-lg overflow-hidden">
+            <Image
+              src="/orbs/orb-2.webp"
+              alt="Logo"
+              width={32}
+              height={32}
+              style={{
+                filter: "hue-rotate(40deg) saturate(1.2)",
+              }}
+              className="size-full object-cover"
+            />
+          </div>
+          <div className="flex flex-col gap-0.5 leading-none">
+            <div className="flex items-center gap-1">
+              <span className="font-medium">LinderLabs</span>
+              <span className="text-primary font-medium">UI</span>
+            </div>
+            <Badge
+              variant="secondary"
+              className="w-fit h-4 px-1 py-0 text-[10px]"
+            >
+              AI
+            </Badge>
+          </div>
+        </Link>
+      </SidebarMenuButton>
     </div>
   );
 }
