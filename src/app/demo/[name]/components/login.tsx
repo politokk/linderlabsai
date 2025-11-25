c8e4cefdd0393404553944183441f410a509b170
politokk <ellielinder2023@gmail.com>
2025-11-25 01:38:48 -0500

    brandheader

--- src/app/demo/[name]/components/login.tsx
+++ src/app/demo/[name]/components/login.tsx
@@ -1,8 +1,11 @@
 import { Login } from "@/components/login";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 export const login = {
   name: "login",
   components: {
-    Default: <Login />,
+    Default:  <SidebarProvider>
+    <Login />
+  </SidebarProvider>
   },
 };
