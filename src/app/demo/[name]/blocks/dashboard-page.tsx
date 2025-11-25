21e458e69d26ddc8e7d875e1421ca9554b7f62db
politokk <ellielinder2023@gmail.com>
2025-11-25 01:32:16 -0500

    brandheader

--- src/app/demo/[name]/blocks/dashboard-page.tsx
+++ src/app/demo/[name]/blocks/dashboard-page.tsx
@@ -4,7 +4,17 @@ export default function DashboardPage() {
   return (
     <div className="flex min-h-[90vh] flex-col items-center justify-center gap-4 rounded-md bg-background">
       <div className="min-w-32 text-muted-foreground">
-  <Hero title="Dashboard" description="Welcome to the dashboard" buttonText="Get Started" buttonLink="/" backgroundImage="/images/dashboard.jpg"  />
+      <Hero 
+  badgeText="Coming soon"
+  title="LinderLabs" 
+  titleAccent="UI"
+  description="AI Tools, Blocks, & Components for your projects." 
+  button1Text="Explore Tools" 
+  button1Link="/tools"
+  button2Text="View Docs"
+  button2Link="/docs"
+  backgroundImage="/images/dashboard.jpg"  
+/>
       </div>
     </div>
   );
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -5,43 +5,51 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 
 export function Hero({
+  badgeText,
   title,
+  titleAccent,
   description,
-  buttonText,
-  buttonLink,
+  button1Text,
+  button1Link,
+  button2Text,
+  button2Link,
   backgroundImage,
 }: {
+  badgeText: string;
   title: string;
+  titleAccent?: string;
   description: string;
-  buttonText: string;
-  buttonLink: string;
+  button1Text: string;
+  button1Link: string;
+  button2Text: string;
+  button2Link: string;
   backgroundImage: string;
 }) {
   return (
-    <div className="relative h-[500px] w-full">
+    <div className="relative h-[480px] w-full">
       <div className="relative z-10 flex h-full flex-col items-center justify-center px-4 text-center sm:px-6 lg:px-8">
         <div className="mx-auto max-w-3xl">
           <Badge variant="secondary" className="mb-4">
-            Coming soon
+            {badgeText}
           </Badge>
-          <h1 className="font-bold text-4xl tracking-tight sm:text-5xl md:text-6xl">
-            {title}
+          <h1 className="font-semibold text-3xl tracking-tight sm:text-3xl md:text-4xl">
+            {title}{titleAccent && <span className="text-primary"> {titleAccent}</span>}
           </h1>
           <p className="mt-6 text-xl">{description}</p>
-          <div className="mt-10 space-x-5">
-          <Button
+          <div className="mt-5 space-x-5">
+            <Button
               asChild
               variant="default"
               size="sm"
             >
-              <Link href={buttonLink}>{buttonText}</Link>
+              <Link href={button1Link}>{button1Text}</Link>
             </Button>
             <Button
               asChild
               variant="outline"
               size="sm"
             >
-              <Link href={buttonLink}>{buttonText}</Link>
+              <Link href={button2Link}>{button2Text}</Link>
             </Button>
           </div>
         </div>
