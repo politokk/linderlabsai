32955b4b5b4aff9b1994ea2aab5e40f163a5d862
politokk <ellielinder2023@gmail.com>
2025-11-25 15:34:52 -0500

    aielements

--- src/app/demo/[name]/index.tsx
+++ src/app/demo/[name]/index.tsx
@@ -4,7 +4,6 @@ import type { ReactElement, ReactNode } from "react";
 import { blank } from "@/app/demo/[name]/blocks/blank";
 import { dashboard } from "@/app/demo/[name]/blocks/dashboard";
 import { store } from "@/app/demo/[name]/blocks/store";
-import SinkPage from "@/app/demo/[name]/blocks/sink/page";
 
 // components
 import { brandHeader } from "@/app/demo/[name]/components/brand-header";
@@ -86,7 +85,6 @@ export const demos: { [name: string]: Demo } = {
   blank,
   store,
   dashboard,
-  sink: SinkPage,
   // components
   hero,
   login,
