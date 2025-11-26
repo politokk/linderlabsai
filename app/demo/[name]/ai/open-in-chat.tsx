468bf3edb3c2af91ef9eecfaf9220807b8e08e3a
politokk <ellielinder2023@gmail.com>
2025-11-26 17:12:52 -0500

    prompt input

---
+++ app/demo/[name]/ai/open-in-chat.tsx
@@ -0,0 +1,13 @@
+import { ComponentWrapper } from "@/components/display/component-wrapper"
+import OpenInChat from "@/components/demos/ai/open-in-chat/open-in-chat";
+export default function openInChat() {
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <ComponentWrapper name="Open in Chat" icon="Bot">
+            <OpenInChat />
+        </ComponentWrapper>
+      </div>
+    </div>
+  )
+}
