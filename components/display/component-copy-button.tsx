bd8b826c114992832f64ff09c664992aed29e794
politokk <ellielinder2023@gmail.com>
2025-11-26 16:28:44 -0500

    promo

--- components/display/component-copy-button.tsx
+++ components/display/component-copy-button.tsx
@@ -29,7 +29,7 @@ export function ComponentCopyButton({
     <Tooltip>
       <TooltipTrigger asChild>
         <Button
-          size="iconSm"
+          size="iconXs"
           variant="ghost"
           onClick={() => {
             navigator.clipboard.writeText(code)
