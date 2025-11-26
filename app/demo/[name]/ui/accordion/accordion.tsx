cb80d255de076bf230add7f726847c6713a0ac0e
politokk <ellielinder2023@gmail.com>
2025-11-26 14:45:13 -0500

    package.json

--- app/demo/[name]/ui/accordion/accordion.tsx
+++ app/demo/[name]/ui/accordion/accordion.tsx
@@ -10,10 +10,10 @@ export default function accordion() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-      <ComponentDisplay path="accordion/accordion-demo" icon={<FolderRootIcon />}>
+      <ComponentDisplay key="accordion-demo" path="accordion/accordion-demo" icon={<FolderRootIcon />}>
               <AccordionDemo />
             </ComponentDisplay>
-            <ComponentDisplay path="accordion/accordion-extensive" icon={<ScrollTextIcon />} >
+            <ComponentDisplay key="accordion-extensive" path="accordion/accordion-extensive" icon={<ScrollTextIcon />} >
               <AccordionExtensive />
             </ComponentDisplay>
       </div>
