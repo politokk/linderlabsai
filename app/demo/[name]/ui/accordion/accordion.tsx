0982ca2461725fc6ec1b99e7a313f9318421c352
politokk <ellielinder2023@gmail.com>
2025-11-26 13:44:27 -0500

    src gone

--- app/demo/[name]/ui/accordion/accordion.tsx
+++ app/demo/[name]/ui/accordion/accordion.tsx
@@ -1,16 +1,13 @@
 import { ComponentWrapper } from "@/components/display/component-wrapper"
 import AccordionDemo from "@/components/demos/accordion/accordion-demo";
 import AccordionExtensive from "@/components/demos/accordion/accordion-extensive";
+import { Preview } from "@/components/code/preview";
 export default function accordion() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <ComponentWrapper name="Accordion" icon="ChevronDown">
-          <AccordionDemo />
-        </ComponentWrapper>
-        <ComponentWrapper name="Accordion Extensive" icon="ScrollText">
-          <AccordionExtensive />
-        </ComponentWrapper>
+        <Preview path="accordion/accordion-demo" />
+        <Preview path="accordion/accordion-extensive" />
       </div>
     </div>
   )
