3cc9e04c1679962e3f19f0768baa1d0fe9d0bd36
politokk <ellielinder2023@gmail.com>
2025-11-26 14:14:34 -0500

    preview and code block tabs

--- app/demo/[name]/ui/accordion/accordion.tsx
+++ app/demo/[name]/ui/accordion/accordion.tsx
@@ -2,12 +2,18 @@ import { ComponentWrapper } from "@/components/display/component-wrapper"
 import AccordionDemo from "@/components/demos/accordion/accordion-demo";
 import AccordionExtensive from "@/components/demos/accordion/accordion-extensive";
 import { Preview } from "@/components/code/preview";
+import { ComponentDisplay } from "@/components/display/component-display";
+import { FolderRootIcon, ScrollTextIcon } from "lucide-react";
 export default function accordion() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <Preview path="accordion/accordion-demo" />
-        <Preview path="accordion/accordion-extensive" />
+      <ComponentDisplay path="accordion/accordion-demo" icon={<FolderRootIcon />}>
+              <AccordionDemo />
+            </ComponentDisplay>
+            <ComponentDisplay path="accordion/accordion-extensive" icon={<ScrollTextIcon />} >
+              <AccordionExtensive />
+            </ComponentDisplay>
       </div>
     </div>
   )
